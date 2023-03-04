40194be1cdb68a58d1a9f6875c980f511ca14c95
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-03-04 15:41:59 -0500

    ADD: Knight class and tests

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
+require_relative './square'
+require_relative './move_manager'
+
 # Knight Class
-
 class Knight
+  attr_reader :moves
 
+  @@movility = [[1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [2, -1], [-2, -1]]
+  
+  def initialize
+    @moves = movility_to_squares
+    @move_manager = MoveManager.new
+  end
+
+  def movility_to_squares
+    @@movility.map do |coordinates|
+      Square.new(coordinates.first, coordinates.last)
+    end
+  end
+
+  def knight_moves(start_position, end_position)
+    moves = @move_manager.find_position(start_position, end_position, @moves)
+    moves = @move_manager.get_path_to_root(moves)
+    moves = @move_manager.print_moves(moves)
+  end
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
 require_relative '../lib/knight'
+require_relative '../lib/square'
 
 describe Knight do
   let(:knight) { Knight.new }
+
+  describe '#movility_to_squares' do
+    it 'transforms the piece movility into square classes' do
+      expect(knight.movility_to_squares.length).to eq(8)
+      expect(knight.movility_to_squares.first).to be_an_instance_of(Square)
+    end
+  end
+
+  describe '#knight_moves' do
+    context 'prints a message with the moves it took' do
+      square = Square.new(3, 3)
+      initial_position = Node.new(square)
+      
+      ending_square = Square.new(4, 3)
+      ending_position = Node.new(ending_square)
+      
+      specify { expect { knight.knight_moves(initial_position, ending_position) }.to output(/You made it in 3 moves! Here's your path:/).to_stdout }
+      specify { expect { knight.knight_moves(initial_position, ending_position) }.to output(/[3, 3]/).to_stdout }
+      specify { expect { knight.knight_moves(initial_position, ending_position) }.to output(/[4, 5]/).to_stdout }
+      specify { expect { knight.knight_moves(initial_position, ending_position) }.to output(/[6, 4]/).to_stdout }
+      specify { expect { knight.knight_moves(initial_position, ending_position) }.to output(/[4, 3]/).to_stdout }
+    end
+  end
 end
